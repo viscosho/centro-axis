c165a80c6bd305e07cccbc2b6b47480d96dc967e
Virginia Garcia <ve.garcia.ortiz@gmail.com>
2020-04-17 19:59:53 -0400

    variables

--- src/components/intro_box.jsx
+++ src/components/intro_box.jsx
@@ -8,14 +8,13 @@ export default class IntroBox extends Component {
 	render() {
 		return (
 			<section className="max-w-4xl flex items-center h-auto lg:h-screen flex-wrap mx-auto my-32 lg:my-0">
-			
+				<img src={logo} className="App-logo" alt="logo" />
 				<div id="profile" className="w-full lg:w-3/5 rounded-lg lg:rounded-l-lg lg:rounded-r-none shadow-2xl bg-white opacity-75 mx-6 lg:mx-0">
 					<div className="p-4 md:p-12 text-center lg:text-left">
 						<div class="block lg:hidden rounded-full shadow-xl mx-auto -mt-16 h-48 w-48 bg-cover bg-center" 
 						style={ {
 							backgroundImage: "url('" + image_therapist + "')"
 						} }></div>
-						<img src={logo} className="App-logo" alt="logo" />
 						<h1 className="text-3xl font-bold pt-8 lg:pt-0">Centro Axis</h1>
 
 						<div className="mx-auto lg:mx-0 w-4/5 pt-3 border-b-2 border-teal-500 opacity-25">
